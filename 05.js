acff8a099d30f624440f1612a7cdd3036c1fad84
jucerocri <jucerocri@hotmail.com>
2023-03-01 08:54:17 -0500

    Desarrollo pre-challenge 05

--- 05.js
+++ 05.js
@@ -13,7 +13,20 @@ function pluck(array, propiedad) {
   // productos.pluck(productos, 'name') debería devolver ['TV LCD', 'Computadora']
   // Pista: es una buena oportunidad para usar map.
 
-  // Tu código acá:
+  // Creamos un nuevo array vacío para almacenar los valores de la propiedad
+  const valores = [];
+
+  // Iteramos sobre cada objeto en el array
+  for (let i = 0; i < array.length; i++) {
+    // Verificamos si el objeto tiene la propiedad solicitada
+    if (array[i].hasOwnProperty(propiedad)) {
+      // Si la tiene, agregamos el valor de la propiedad al nuevo array
+      valores.push(array[i][propiedad]);
+    }
+  }
+
+  // Devolvemos el nuevo array con los valores de la propiedad
+  return valores;
 
 }
 
