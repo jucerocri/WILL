426fc02f2947d1be940082d0961e521787475453
jucerocri <jucerocri@hotmail.com>
2023-03-01 08:54:57 -0500

    Desarrollo pre-challenge 06-07-08

--- 06-07-08.js
+++ 06-07-08.js
@@ -8,50 +8,56 @@ function crearClasePersona() {
   class Persona {
     constructor(nombre, edad, hobbies, amigos) {
       // El constructor de la clase Persona recibe nombre (string), edad (integer), hobbies (array de strings), amigos (array de objetos)
-      // Inicializar las propiedades de la persona con los valores recibidos como argumento
-
-      // Tu código aca:
-
+      // Inicializar las propiedades de la persona con los valores recibidos como argumento  
+      this.nombre= nombre;
+      this.edad= edad;
+      this.hobbies= hobbies; //this.hobbies = hobbies && hobbies.every(hobbie => typeof hobbie === 'string') ? hobbies : [];
+      this.amigos= amigos;
     }
 
-    addFriend(nombre, edad) {
+//El método every() de JavaScript funciona en arreglos y lo que hace es verificar si todos los elementos de un arreglo pasan un test. El test es una función.
+//El método every() comienza a ejecutar la función de test para cada elemento del arreglo y va a retornar false o true, según sea el caso:
+//Falso: el método retornara false si es que la función de test retorne false para algún elemento y luego de eso dejará de ejecutar la función test en los siguientes elementos.
+//Verdadero: si la función de test no retornar ningún valor false, en ese caso el método every() devolverá true. Hay que tener en cuenta que este método no cambia el arreglo original.    
+
+
+
+    addFriend(nombre, edad) {    
       // El método 'addFriend' recibe un string 'nombre' y un entero 'edad' y debe agregar un objeto:
       // { nombre: nombre, edad: edad} al arreglo de amigos de la persona.
       // No debe retornar nada.
-
-      // Tu código aca:
-
+      
+      let amigo = {nombre, edad}
+      this.amigos.push(amigo);
     }
 
-    addHobby(hobby) {
+    addHobby(hobby) {     
       // El método 'addHobby' recibe un string 'hobby' y debe agregarlo al arreglo de hobbies de la persona.
       // No debe retornar nada.
-
-      // Tu código aca:
-
+      this.hobbies.push(hobby);
     }
-    getFriends() {
+
+    getFriends() {     
       // El método 'getFriends' debe retornar un arreglo con sólo los nombres del arreglo de amigos
       // de la persona.
       // Ej:
       // Suponiendo que la persona tiene estos amigos: [{nombre: 'martin', edad: 31},{nombre: 'toni', edad: 33}]
       // persona.getFriends() debería devolver ['martin', 'toni']
 
-      // Tu código aca:
-
+      const transformarObjetoAmigoANombre = ((amigo) => amigo.nombre);
+      const indexed = this.amigos.map(transformarObjetoAmigoANombre);
+      return indexed
     }
 
     getHobbies() {
       // El método 'getHobbies' debe retornar un arreglo con los hobbies de la persona
       // Ej:
       // persona.getHobbies() debe devolver ['correr', 'dormir', 'nadar']
-
-      // Tu código aca:
-
+      return this.hobbies
     }
 
     getPromedioEdad() {
-      // El método 'getPromedioEdad' debe retornar el promedio de edad de los amigos de una persona
+            // El método 'getPromedioEdad' debe retornar el promedio de edad de los amigos de una persona
       // Ej:
       // Si la persona tuviera estos amigos:
       // {
@@ -65,14 +71,12 @@ function crearClasePersona() {
       // }
       // persona.getPromedioEdad() debería devolver 29 ya que (33 + 25) / 2 = 29
 
-      // Tu código aca:
+      return this.amigos.map(amigo => amigo.edad).reduce((a, b) => a + b) / this.amigos.length
 
     }
-  };
-
+  }
   return Persona;
 }
-
 // No modifiques nada debajo de esta linea //
 
 module.exports = crearClasePersona
