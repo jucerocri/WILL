c5b3566fc39de5b9b94dbe1cd8a720db3a1e2c5c
jucerocri <jucerocri@hotmail.com>
2023-03-01 08:53:59 -0500

    Desarrollo pre-challenge 04

--- 04.js
+++ 04.js
@@ -12,7 +12,15 @@ function numeroSimetrico(num) {
   // Ej:
   // numeroSimetrico(11711) devuelve true
 
-  // Tu código:
+  const numInvertido = parseInt(num.toString().split('').reverse().join(''));
+  return num === numInvertido;
+
+  //toString()  --> Convierte el número en una cadena de caracteres.
+  //split('')   --> Divide la cadena en un arreglo de caracteres.
+  //reverse()   --> Inverte el orden de los caracteres.
+  //join('')    --> Une los caracteres de nuevo en una cadena. 
+  //parseInt()  --> Convierte la cadena invertida en un número. 
+  //Finalmente  --> compara el número original con el número invertido, y devuelve true si son iguales, y false si son diferentes.
 
 }
 
